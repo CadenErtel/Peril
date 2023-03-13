8b69fd46098f6efcf584a4d6ec19aa3a9a8bf218
Caden Ertel <cadenertel@gmail.com>
2023-03-12 22:41:31 -0400

    clean up of files, added atlas

--- src/scenes/TitleScene.ts
+++ src/scenes/TitleScene.ts
@@ -7,41 +7,21 @@ export default class TitleScene extends Phaser.Scene {
 
     preload () {
         this.load.image('title', "/images/title.png");
-        this.load.image('bg1', "/images/title_bg_1.png");
-        this.load.image('bg2', "/images/title_bg_2.png");
-        this.load.image('bg3', "/images/title_bg_3.png");
-        this.load.image('bg4', "/images/title_bg_4.png");
+        this.load.image('background', '/images/title_bg.png');
 
-        this.load.image('button-up', "/images/host-button-up.png");
-        this.load.image('button-down', "/images/host-button-down.png");
+        this.load.atlas('atlas', '/atlas/title/buttons.png', '/atlas/title/buttons.json');
         this.load.audio('button-press-sound', '/audio/button-press.mp3');
 
     }
 
     create() {
 
-        const button_press_sound = this.sound.add('button-press-sound');
-
-        for (let i = 0; i < 4; i++){
-            let image = this.add.image(0, 0,`bg${i+1}`).setOrigin(0,0);
-            image.displayWidth = this.sys.canvas.width;
-            image.displayHeight = this.sys.canvas.height;
-        }
+        let image = this.add.image(0, 0, 'background').setOrigin(0,0);
+        image.displayWidth = this.sys.canvas.width;
+        image.displayHeight = this.sys.canvas.height;
         
         this.add.image(window.innerWidth / 2, window.innerHeight / 6, 'title').scale = 1.5;
         
-        // Define the "button-anim" animation with the "button-up" and "button-down" frames
-        this.anims.create({
-            key: 'button-press',
-            frames: [
-                { key: 'button-up' },
-                { key: 'button-down' },
-                { key: 'button-up'}
-            ],
-            frameRate: 45,
-            repeat: 0
-        });
-
         const input = this.add.dom(2 * window.innerWidth / 3, 2 * window.innerHeight / 5, 'input');
         input.node.setAttribute('id', 'join-game-field');
         
@@ -53,31 +33,51 @@ export default class TitleScene extends Phaser.Scene {
                 (input.node as HTMLInputElement).value = '';
             }
         });
+
+        const button_press_sound = this.sound.add('button-press-sound');
         
-        const button1 = this.add.sprite(window.innerWidth / 3, window.innerHeight / 2, 'button-up').setInteractive();
+        const button1 = this.add.sprite(window.innerWidth / 3, window.innerHeight / 2, 'atlas', 'host-button-up').setInteractive();
         
         button1.on('pointerdown', () => {
-            button1.anims.play("button-press");
             button_press_sound.play();
+            buttonPress('host', button1);
             fadeOut('gamestart', this);
         });
-
-        const button2 = this.add.sprite(2 * window.innerWidth / 3, window.innerHeight / 2, 'button-up').setInteractive();
+        
+        const button2 = this.add.sprite(2 * window.innerWidth / 3, window.innerHeight / 2, 'atlas', 'join-button-up').setInteractive();
         button2.on('pointerdown', () => {
             button2.anims.play("button-press");
-            button_press_sound.play();
+            buttonPress('join', button2);
             fadeOut('gamestart', this);
         });
+        
+        const buttonPress = (button_name : string, currentButton : Phaser.GameObjects.Sprite) => {
 
+            // Define the "button-anim" animation with the "button-up" and "button-down" frames
+            currentButton.anims.create({
+                key: 'button-press',
+                frames: [
+                    { key: 'atlas' , frame : `${button_name}-button-up`},
+                    { key: 'atlas' , frame : `${button_name}-button-down`},
+                    { key: 'atlas' , frame : `${button_name}-button-up`}
+                ],
+                frameRate: 45,
+                repeat: 0
+            });
+
+            currentButton.anims.play("button-press");
+
+        }
+        
         const fadeOut = (nextScene : string, currentScene : Phaser.Scene) => {
             currentScene.cameras.main.once(Phaser.Cameras.Scene2D.Events.FADE_OUT_COMPLETE, function () {
                 console.log('Fade out complete for button:', nextScene);
                 currentScene.scene.start(nextScene, { fadeIn: true });
-            });
-        
+            }, currentScene);
+            
             currentScene.cameras.main.fadeOut(250);
         }
-
+        
         
     }
 }
